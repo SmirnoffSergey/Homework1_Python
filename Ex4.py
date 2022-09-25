7d57f4041149d140df14eade2f127812aa5abb28
Sergey <s_smirnov@nisse.ru>
2022-09-25 16:55:07 +0300

    fixed Ex4.py

--- Ex4.py
+++ Ex4.py
@@ -2,15 +2,15 @@
 # диапазон возможных координат точек в этой четверти (x и y).
 
 print('Enter the number of the quarter plane from 1 to 4:')
-numberQuarter = int(input())
+number_of_the_quarter = int(input())
 print()
 
-while numberQuarter < 1 or numberQuarter > 4:
+while number_of_the_quarter < 1 or number_of_the_quarter > 4:
     print('Enter the number of the quarter plane from 1 to 4:')
-    numberQuarter = int(input())
+    number_of_the_quarter = int(input())
 print()
 
-if numberQuarter == 1: print('The coordinates by axes are: x > 0 and y > 0')
-if numberQuarter == 2: print('The coordinates by axes are: x < 0 and y > 0')
-if numberQuarter == 3: print('The coordinates by axes are: x < 0 and y < 0')
-if numberQuarter == 4: print('The coordinates by axes are: x > 0 and y < 0')
+if number_of_the_quarter == 1: print('The coordinates by axes are: x > 0 and y > 0')
+if number_of_the_quarter == 2: print('The coordinates by axes are: x < 0 and y > 0')
+if number_of_the_quarter == 3: print('The coordinates by axes are: x < 0 and y < 0')
+if number_of_the_quarter == 4: print('The coordinates by axes are: x > 0 and y < 0')
